f56eb1f7612f9e16ab5f0ca8578b0c27f52ba785
iraklisarkisovi <sarqisovirakli@gmail.com>
2024-09-10 12:31:54 +0400

    Commit

--- src/Components/HomeworksList.jsx
+++ src/Components/HomeworksList.jsx
@@ -1,148 +1,105 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import HomeworkItem from "./HomeworksItem"; 
 
-class HomeworksList extends Component {
-    state = {
-        inputValue: '',
-        homeworks: [], 
-        submittedHomeworks: [] 
-    }
+const HomeworksList = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [homeworks, setHomeworks] = useState([]);
+    const [submittedHomeworks, setSubmittedHomeworks] = useState([]);
 
-    onChange = (event) => {
-        this.setState({
-            inputValue: event.target.value
-        });
-    }
+    const onChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-    addHomework = (event) => {
+    const addHomework = (event) => {
         event.preventDefault();
-
-        if (this.state.inputValue.trim() === '') return; 
+        if (inputValue.trim() === '') return;
 
         const newHomework = {
-            id: this.state.homeworks.length + 1,  
-            name: this.state.inputValue
+            id: homeworks.length + 1,
+            name: inputValue
         };
 
-        this.setState((prevState) => ({
-            homeworks: [...prevState.homeworks, newHomework],
-            inputValue: ''
-        }));
-    }
+        setHomeworks(prevHomeworks => [...prevHomeworks, newHomework]);
+        setInputValue('');
+    };
 
-    // removeHomework = (id) => {
-    //     this.setState((prevState) => ({
-    //         homeworks: prevState.homeworks.filter(homework => homework.id !== id)
-    //     }));
-    // }
-
-    submitHomework = (id) => {
-        this.setState((prevState) => {
-
-            const homeworkToSubmit = prevState.homeworks.find(homework => homework.id === id);
-
-            return {
-                homeworks: prevState.homeworks.filter(homework => homework.id !== id),
-                submittedHomeworks: [...prevState.submittedHomeworks, homeworkToSubmit]
-            };
-        });
-    }
-
-    removeSubmittedHomework = (id) => {
-        this.setState((prevState) => {
-
-            return {
-                submittedHomeworks: prevState.submittedHomeworks.filter(homework => homework.id !== id),
-            };
-        });
-    }
-
-    unsubmitHomework = (id) => {
-        this.setState((prevState) => {
-            const homeworkToUnsubmit = prevState.submittedHomeworks.find(homework => homework.id === id);
-
-            return {
-                submittedHomeworks: prevState.submittedHomeworks.filter(homework => homework.id !== id),
-                homeworks: [...prevState.homeworks, homeworkToUnsubmit]
-            };
-        });
-
-    }
-
-    handleAlert = (Callback) => {
-        alert(Callback);
-    }
-
-    shouldComponentUpdate(nextState) {
-        console.log("rerender")
-        if (this.state.homeworks.id !== nextState.homeworks || this.state.submittedHomeworks !== nextState.submittedHomeworks) {
-            return true; 
+    const submitHomework = useCallback((id) => {
+        setHomeworks(prevHomeworks => {
             
-        }
-        return false; 
-    }
+            const homeworkToSubmit = prevHomeworks.find(homework => homework.id === id);
 
-    render() {
-        return (
-            <div className="homeworks">
-                <h3>Create and solve homework</h3>
+            setSubmittedHomeworks(prevSubmitted => [...prevSubmitted, homeworkToSubmit]);
 
-                <br />
-                <form onSubmit={this.addHomework}>
-                    <input
-                        type="text"
-                        value={this.state.inputValue}
-                        onChange={this.onChange}
+            return prevHomeworks.filter(homework => homework.id !== id);
+        });
+    }, []);
+
+    const removeSubmittedHomework = useCallback((id) => {
+        setSubmittedHomeworks(prevSubmitted => prevSubmitted.filter(homework => homework.id !== id));
+    }, []);
+
+    const unsubmitHomework = useCallback((id) => {
+        setSubmittedHomeworks(prevSubmitted => {
+            const homeworkToUnsubmit = prevSubmitted.find(homework => homework.id === id);
+            setHomeworks(prevHomeworks => [...prevHomeworks, homeworkToUnsubmit]);
+            return prevSubmitted.filter(homework => homework.id !== id);
+        });
+    }, []);
+
+    const handleAlert = useCallback((message) => {
+        alert(message);
+    }, []);
+
+    return (
+        <div className="homeworks">
+            <h3>Create and solve homework</h3>
+
+            <form onSubmit={addHomework}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={onChange}
+                />
+                <button type="submit" className="firstBtn">Add homework</button>
+            </form>
+
+            <div className="homework-list">
+                <h4>Unsubmitted Homeworks</h4>
+
+                {homeworks.map(homework => (
+                    <HomeworkItem
+                        key={homework.id}
+                        id={homework.id}
+                        name={homework.name}
+                        actionRemove={() => handleAlert(`Submit this homework first: ${homework.name} id: ${homework.id}`)}
+                        actionSubmit={submitHomework}
+                        unsubHW={() => handleAlert(`Submit this homework first: ${homework.name} id: ${homework.id}`)}
                     />
-                    <button type="submit" className="firstBtn">Add homework</button>
-                </form>
+                ))}
                 <br />
-
-                <div className="homework-list">
-                    <br />
-                    <h4>Unsubmitted Homeworks</h4>
-                    <br />
-                  
-
-                    {this.state.homeworks.map(homework => (
-                        <HomeworkItem
-                            key={homework.id}
-                            id={homework.id}
-                            name={homework.name}
-                            actionRemove={() => {
-                                this.handleAlert(`Submit this homework first: ${homework.name} id: ${homework.id}`);
-                            }}
-                            actionSubmit={this.submitHomework}
-                            unsubHW={() => {
-                                this.handleAlert(`Submit this homework first: ${homework.name} id: ${homework.id}`);
-                            }}
-                        />
-                    ))}  
-                    <hr />
-                    <br />
-                </div>
-               
-                <div className="submitted-homeworks">
-                    <br />
-                    <h4>Submitted Homeworks</h4>
-                    <br />
-           
-                    {this.state.submittedHomeworks.map(homework => (
-                        <HomeworkItem
-                            key={homework.id}
-                            id={homework.id}
-                            name={homework.name}
-                            actionSubmit={() => {this.handleAlert(`Unsubmit this homework first: ${homework.name} id: ${homework.id}`)}}
-                            actionRemove={this.removeSubmittedHomework}
-                            unsubHW={this.unsubmitHomework}
-                        />
-                    ))}
-                    <hr />
-                    <br />
-                </div>
+                <hr />
+                <br />
             </div>
-        );
-    }
-}
+
+            <div className="submitted-homeworks">
+                <h4>Submitted Homeworks</h4>
+
+                {submittedHomeworks.map(homework => (
+                    <HomeworkItem
+                        key={homework.id}
+                        id={homework.id}
+                        name={homework.name}
+                        actionSubmit={() => handleAlert(`Unsubmit this homework first: ${homework.name} id: ${homework.id}`)}
+                        actionRemove={removeSubmittedHomework}
+                        unsubHW={unsubmitHomework}
+                    />
+                ))}
+                <br />
+                <hr />
+                <br />
+            </div>
+        </div>
+    );
+};
 
 export default HomeworksList;
